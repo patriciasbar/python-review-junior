b0760bcae5fde0b349d46e4851d7dd8d8a7c4a4a
patriciasbar <104311967+patriciasbar@users.noreply.github.com>
2025-04-29 09:03:45 +0100

    few updates

--- source/list_comprehension.py
+++ source/list_comprehension.py
@@ -7,15 +7,15 @@ If/Else Nested: [expression_if_true if condition else expression_if_false for it
 """
 
 # Create a list of the squares of numbers from 0 to 9.
-squared_nums = [x**2 for x in range(0,9)]
+squared_nums = [x**2 for x in range(0,10)]
 #print(squared_nums)
 
 #Make a list containing all letters in the word "Gratitude".
-letters_list = [word for word in ("Gratitude")]
+letters_list = [word for word in "Gratitude"]
 #print(letters_list)
 
 #For numbers 0 to 9, create a list where each number is replaced by "big" if it is greater than 5, otherwise "small".
-nums_list = ["big" if x > 5 else "small" for x in range(0,9)]
+nums_list = ["big" if x > 5 else "small" for x in range(0,10)]
 #print(nums_list)
 
 #Given a list of temperatures in Celsius, make a new list with "hot" if the temperature is over 30, otherwise "cool".
@@ -24,7 +24,7 @@ temperature_celsius = ["hot" if i > 30 else "cool" for i in range(0,100, 15)]
 
 
 #Create a list of all possible sums of two numbers, where the first number is from 0 to 2 and the second number is from 0 to 3.
-sum_of_combinations = [x+y for x in range(0,2) for y in range(0,3)]
+sum_of_combinations = [x+y for x in range(3) for y in range(4)]
 #print(sum_of_combinations)
 
 #Make a list of all pairs (x, y) where x is from "abc" and y is from "12".
@@ -32,14 +32,14 @@ pair_combinations = [(x,y) for x in ("abc") for y in ("12")]
 #print(pair_combinations)
 
 #Create a list of (x, y) if x is not equal to y; otherwise, put "match" for x from 0 to 2 and y from 0 to 2.
-list_matches = [(x,y) if x!=y else ("match", y) for x in range(0,2) for y in range(0,2)]
+list_matches = [(x,y) if x!=y else (x, "match") for x in range(3) for y in range(3)]
 #print(list_matches)
 
 #Make a list where for each combination of day and temperature # (days = ["Mon", "Tue"], temps = [20, 35]), 
 # you put (day, "hot") if temp > 30, else (day, "cool").
 days = ["Mon", "Tue"]
 temps = [20,35]
-list_days_temps = [(day, "hot") if temp > 30 else (day, "cool") for day in days for temp in temps] 
+list_days_temps = [(day, "hot") if temp > 30 else (day, "cool") for day, temp in zip(days, temps)] 
 #print(list_days_temps)
 
 
